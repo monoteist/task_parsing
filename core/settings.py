1777fa3b5ff8a30f98f75666ee0aa693366f6504
monoteist <i.ezerbaev97@gmail.com>
2021-10-06 13:23:07 +0300

    update requirements.txt | add sort for table | add paginate | add search in table | add cache | add Readme

--- core/settings.py
+++ core/settings.py
@@ -125,3 +125,11 @@ STATICFILES_DIRS = ((BASE_DIR / 'static'),)
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
--- scrapping/views.py
+++ scrapping/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
 from django.views import View
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from django.core.cache import cache
 from .parsing import result
+
+
 
 class HomePage(View):
     def get(self, request):
-        return render(request, 'index.html')
-
-    def post(self, request):
-        url = request.POST.get('url')
-        scrapping_data = result(url)
-        page = 1
-        paginator = Paginator(scrapping_data, 1)
-        data = paginator.page(page).object_list[0]
-        return render(request, 'result.html', {'data': data})
+        url = request.GET.get('url')
+        if url:
+            if cache.get('data'):
+                page = request.GET.get('page', 1)
+                paginator = Paginator(cache.get('data'), 1)
+                data = paginator.page(page)
+            else:
+                scrapping_data = result(url)
+                page = request.GET.get('page', 1)
+                paginator = Paginator(scrapping_data, 1)
+                data = paginator.page(page)
+                cache.set('data', scrapping_data, 60 * 15)
+            return render(request, 'result.html', {'data': data, 'url': url})
+        return render(request, 'index.html')  
         
